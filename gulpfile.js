efbf51b3e9364f5349eef159f35de8eab103aa18
Jonathan Duran <headjd@gmail.com>
2017-03-29 09:12:48 -0400

    Add Sass

--- gulpfile.js
+++ gulpfile.js
@@ -13,15 +13,6 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
 
-  var npmDir = 'node_modules/',
-    jsDir = 'resources/assets/js/';
-
-  mix.copy(npmDir + 'vue/dist/vue.min.js', jsDir);
-  mix.copy(npmDir + 'vue-resource/dist/vue-resource.min.js', jsDir);
-
-  mix.scripts([
-    'vue.min.js',
-    'vue-resource.min.js'
-  ], 'public/js/vendor.js');
+  mix.sass('main.scss');
 
 });
